ff47ea0d35a23bd2725ed94343482f3b9662e1a5
pilarariassantos <pilarmaria15@gmail.com>
2022-07-15 20:33:07 -0300

    mi primer commit proyecto 2

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const menuItems = document.querySelectorAll('.menu-item');
+const menuBtn = document.querySelector('#menu-button');
+
+menuBtn.addEventListener('click', function(){
+document.body.classList.toggle('mobile-menu-active');
+});
+
+menuItems.forEach(function(item) {
+    item.addEventListener('click',function(){
+        document.body.classList.remove('mobile-menu-active')
+        const currentItem = document.querySelector('.active');
+        currentItem.classList.remove('active');
+        this.classList.add('active');
+        });
+    });
